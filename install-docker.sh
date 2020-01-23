db145742eb4dc12ce823049cac6d5a0adce8ac12
Siv S <sivsivsree@gmail.com>
2020-01-23 14:31:55 +0400

    pull and install

--- install-docker.sh
+++ install-docker.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+curl -O https://download.docker.com/linux/static/stable/x86_64/docker-19.03.5.tgz
+sudo mv docker-19.03.5.tgz ./binaries/docker-19.03.5.tgz
+tar xvf ./binaries/docker-19.03.5.tgz
+sudo rm -f ./binaries/docker-19.03.5.tgz
+
+curl -O https://github.com/docker/compose/releases/download/1.25.1/docker-compose-$(uname -s)-$(uname -m) -kL
+sudo mv docker-compose-$(uname -s)-$(uname -m) ./binaries/docker-compose-$(uname -s)-$(uname -m)
+
 sudo cp binaries/* /usr/bin/ && sudo dockerd &
 sudo chmod +x /usr/local/bin/docker-compose
 
